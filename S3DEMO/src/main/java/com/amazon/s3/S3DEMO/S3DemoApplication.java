3830ca2a4fe9ee76683c8bc6ee8efc28668d1569
billion-dollar <billion-dollar@Genuine>
2018-09-03 21:56:15 +0530

    First Commit with files

---
+++ S3DEMO/src/main/java/com/amazon/s3/S3DEMO/S3DemoApplication.java
@@ -0,0 +1,12 @@
+package com.amazon.s3.S3DEMO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3DemoApplication.class, args);
+	}
+}
---
+++ S3DEMO/src/test/java/com/amazon/s3/S3DEMO/S3DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.amazon.s3.S3DEMO;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class S3DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
